c127ba12375523a61931efa986aa76f650abe576
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-17 14:39:40 +0200

    create first class and other

---
+++ index.php
@@ -0,0 +1,58 @@
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+
+
+<?php
+
+/*
+
+Immaginiamo le classi per modellizzare un personal computer.
+Un computer desktop é un computer.
+Un computer portatile é un computer.
+Creiamo la classe computer come parent class ed estendiamola per le classi desktop e laptop.
+Creiamo un set di dati in forma di array di oggetti e stampiamoli a schermo in una card usando bootstrap.
+Nella card, indichiamo anche che tipo di prodotto stiamo visualizzando (desktop, laptop) creando un apposito metodo poliforfo in ciascuna sottoclasse.
+
+*/
+
+class Computer
+{
+
+    public $brand;
+    public $model;
+    public $price;
+
+    public function __construct($brand, $model, $price)
+    {
+        $this->brand = $brand;
+        $this->model = $model;
+        $this->price = $price;
+    }
+
+    public function getType()
+    {
+        return "Computer";
+    }
+
+    public function getBrand()
+    {
+        return $this->brand;
+    }
+
+    public function getModel()
+    {
+        return $this->model;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
+
+?>
