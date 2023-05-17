3c7f0403409aaa2bd2b6d3a546af3fa0606d9ba6
edoardorizzo <edo96.rizzo@gmail.com>
2023-05-17 14:48:18 +0200

    add son function to computer

--- index.php
+++ index.php
@@ -53,6 +53,14 @@ class Computer
     {
         return $this->price;
     }
+};
+class Desktop extends Computer
+{
+    public function getType()
+    {
+        return "Desktop";
+    }
 }
+
 
 ?>
